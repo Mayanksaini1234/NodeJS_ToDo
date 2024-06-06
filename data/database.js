b0abfec1178b21bcb7501d59d1f49e87421b6fe9
Mayank <mayanksaini9389@gmail.com>
2024-06-06 15:48:46 +0530

    commit

--- data/database.js
+++ data/database.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 export const connectDb = ()=>{
-    mongoose.connect(process.env.mongo_URL).then(() => {
-  console.log("conneted to mongodb ");
+    mongoose.connect(process.env.mongo_URL).then((c) => {
+  console.log(`conneted to mongodb ${c.connection.host}` );
 })
   .catch((err) => {
     console.error(err);
